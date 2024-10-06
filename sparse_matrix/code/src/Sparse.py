40c1f11172be9529a14fccb0565669ee18abe15e
Eistein <didganza7@gmail.com>
2024-10-06 20:15:50 +0300

    changes in main file

--- sparse_matrix/code/src/Sparse.py
+++ sparse_matrix/code/src/Sparse.py
@@ -29,44 +29,44 @@ class SparseMatrix:
         except Exception as e:
             raise ValueError("Input file has wrong format") from e
 
-    def getvalue(self, currRow, currCol):
+    def getElement(self, currRow, currCol):
         return self.matrix.get((currRow, currCol), 0)
 
-    def setvalue(self, currRow, currCol, value):
+    def setElement(self, currRow, currCol, value):
         if value != 0:
             self.matrix[(currRow, currCol)] = value
         elif (currRow, currCol) in self.matrix:
             del self.matrix[(currRow, currCol)]
 
-    def add(self, other):
+    def add_matrix(self, other):
         if self.numRows != other.numRows or self.numCols != other.numCols:
             raise ValueError("Matrices dimensions do not match for addition")
 
         result = SparseMatrix(numRows=self.numRows, numCols=self.numCols)
         for (row, col), value in self.matrix.items():
-            result.setvalue(row, col, value + other.getvalue(row, col))
+            result.setElement(row, col, value + other.getElement(row, col))
 
         for (row, col), value in other.matrix.items():
             if (row, col) not in self.matrix:
-                result.setvalue(row, col, value)
+                result.setElement(row, col, value)
 
         return result
 
-    def subtract(self, other):
+    def sub_matrix(self, other):
         if self.numRows != other.numRows or self.numCols != other.numCols:
             raise ValueError("Matrices dimensions do not match for subtraction")
 
         result = SparseMatrix(numRows=self.numRows, numCols=self.numCols)
         for (row, col), value in self.matrix.items():
-            result.setvalue(row, col, value - other.getvalue(row, col))
+            result.setElement(row, col, value - other.getElement(row, col))
 
         for (row, col), value in other.matrix.items():
             if (row, col) not in self.matrix:
-                result.setvalue(row, col, -value)
+                result.setElement(row, col, -value)
 
         return result
 
-    def multiply(self, other):
+    def mul_matrix(self, other):
         if self.numCols != other.numRows:
             raise ValueError("Number of columns in the first matrix must equal the number of rows in the second matrix")
 
@@ -74,7 +74,7 @@ class SparseMatrix:
 
         for (i, j), val in self.matrix.items():
             for k in range(other.numCols):
-                result.setvalue(i, k, result.getvalue(i, k) + val * other.getvalue(j, k))
+                result.setElement(i, k, result.getvalue(i, k) + val * other.getElement(j, k))
 
         return result
 
@@ -82,7 +82,7 @@ class SparseMatrix:
         for row in range(self.numRows):
             for col in range(self.numCols):
                 # Print each element with a tab separating columns
-                print(self.getvalue(row, col), end="\t")
+                print(self.getElement(row, col), end="\t")
             print()  # Move to the next line after each row
 
     def print_sparse_matrix(self):
@@ -93,18 +93,18 @@ class SparseMatrix:
 # Example usage:
 if __name__ == "__main__":
     
-    matrix1 = SparseMatrix("../../sample_inputs/easy_sample_02_2.txt")
-    matrix2 = SparseMatrix("../../sample_inputs/easy_sample_02_1.txt")
+    matrix = SparseMatrix("../../sample_inputs/easy_sample_02_2.txt")
+    matrix_1 = SparseMatrix("../../sample_inputs/easy_sample_02_1.txt")
 
     operation = input("Select operation: add, subtract, multiply: ")
 
     if operation == "add":
-        result = matrix1.add(matrix2)
+        result = matrix.add_matrix(matrix_1)
         print(result)
     elif operation == "subtract":
-        result = matrix1.subtract(matrix2)
+        result = matrix.sub_matrix(matrix_1)
     elif operation == "multiply":
-        result = matrix1.multiply(matrix2)
+        result = matrix.mul_matrix(matrix_1)
     else:
         print("Invalid operation selected")
         exit()
